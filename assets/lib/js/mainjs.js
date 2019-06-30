a3a8e6fd6b197b0609efebd642079a152a263539
Andrew-Wyn <terraformer.144@gmail.com>
2019-06-30 17:58:00 +0200

    colorfull

--- assets/lib/js/mainjs.js
+++ assets/lib/js/mainjs.js
@@ -1,17 +1,3 @@
 $(document).ready(function(){
-    ScrollReveal({ reset: true });
-    ScrollReveal().reveal('.headline');			// jQuery methods go here...
 
-    ScrollReveal().reveal('.headline', { 
-        delay: 1000,
-        distance: '30px',
-        origin: 'right'
-    });
-    ScrollReveal().reveal('.container');			// jQuery methods go here...
-
-    ScrollReveal().reveal('.container', { 
-        delay: 1000,
-        distance: '30px',
-        origin: 'bottom'
-    });
 });
